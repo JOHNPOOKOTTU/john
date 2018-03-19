d3034cb3f9dafb68079ae73cfe26883b8b101293
JOHNPOOKOTTU <johnpookottu2020@cs.ajce.in>
2018-03-19 11:21:39 +0530

    this is my commit :3

---
+++ new.c
@@ -0,0 +1,18 @@
+echo "current logged user details---- " 
+w -h
+echo "your current shell"
+echo $0
+
+
+echo "CURRENT HOME DIRECTORY----"$HOME
+echo "OSTYPE--"
+uname -v
+echo "your current pathsetting--"
+$PATH
+echo "your current working directory-"
+pwd
+echo " no of users--"
+w -u
+
+
+
